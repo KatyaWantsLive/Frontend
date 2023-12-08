7d6abf4f07ec53b40f677b7667890d43777d7319
ChikiPiki <chikipiki@MacBook-Air-ChikiPiki.local>
2023-12-09 00:13:06 +0300

    login compleate

--- pages/LoginAndRegistration/Components/LoginForm.js
+++ pages/LoginAndRegistration/Components/LoginForm.js
@@ -1,13 +1,29 @@
 import { View, TextInput, Button } from "react-native";
 import styleLoginForm from "./styles/styleLoginForm";
+import axios from "axios";
+import { useState } from "react";
 
 
 const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const HandlerLogin = async () => {
+        try {
+            const response = await axios.post('https://jhdg-kjhdghj.onrender.com/user/login', {
+                "username" : username,
+                "password" : password
+            });
+            console.log('Ура мы молодцы', response.status);
+        } catch (error) {
+            console.error('Ошибка =(', error)
+        }
+    }
     return (
         <View style={styleLoginForm.container}>
-            <TextInput placeholder="username" style={styleLoginForm.input}></TextInput>
-            <TextInput placeholder="password" style={styleLoginForm.input}></TextInput>
-            <View style={styleLoginForm.loginBtn}><Button title="Войти" style={styleLoginForm.loginBtn} color={'black'}></Button></View>
+            <TextInput placeholder="username" style={styleLoginForm.input} onChangeText={setUsername}></TextInput>
+            <TextInput placeholder="password" style={styleLoginForm.input} onChangeText={setPassword}></TextInput>
+            <View style={styleLoginForm.loginBtn}><Button title="Войти" style={styleLoginForm.loginBtn} color={'black'} onPress={HandlerLogin}></Button></View>
         </View>
     )
 }
